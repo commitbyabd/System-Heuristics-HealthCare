4d59e424a7b651a34467b3463e42cbbf4a8d5314
muhammad-abdullah777 <muhammadabdullahamjad@gmail.com>
2026-04-20 16:55:49 +0500

    Add Under Maintanence Page

---
+++ src/data/under-maintenance/UnderMaintenanceData.jsx
@@ -0,0 +1,10 @@
+export const UnderMaintenanceData = {
+  hero: {
+    title: "Site Under Maintenance",
+    description:
+      "We are currently performing scheduled maintenance. Please check back later.",
+
+    logo: `/logo.svg`,
+    image: "/images/pages/under-maintenance/under-maintenance.svg", // optional: path to an image
+  },
+};
