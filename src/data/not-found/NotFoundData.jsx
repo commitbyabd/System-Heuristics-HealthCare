6f7f3d40989f94f5e169425f0cd4cf4b1e0e960f
muhammad-abdullah777 <muhammadabdullahamjad@gmail.com>
2026-04-20 16:54:49 +0500

    Add not found page

---
+++ src/data/not-found/NotFoundData.jsx
@@ -0,0 +1,8 @@
+export const NotFoundData = {
+  hero: {
+    title: "",
+    description:
+      "The page you are looking for does not exist. You may have mistyped the URL or the page has been moved.",
+    image: "/images/pages/not-found/not-found.svg",
+  },
+};
