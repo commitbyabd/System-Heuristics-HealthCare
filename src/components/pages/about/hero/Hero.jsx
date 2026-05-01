799a65ce6a55fc4987eb2016c49a599251c512e5
muhammad-abdullah777 <muhammadabdullahamjad@gmail.com>
2026-05-01 14:44:37 +0500

    hero section of the about page made successfully

--- src/components/pages/about/hero/Hero.jsx
+++ src/components/pages/about/hero/Hero.jsx
@@ -1,22 +1,30 @@
 import styles from "./hero.module.css";
+import Container from "../../../ui/container/Container";
+import SectionIntro from "../../../ui/section-intro/SectionIntro";
+import Button from "../../../ui/button/Button";
+import SharedGradient from "../../home/shared-gradient/SharedGradient";
 
 function Hero() {
   return (
-    <section className={styles.hero}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>About Us</h1>
-        <p className={styles.description}>
-          We are a healthcare company dedicated to providing high-quality
-          medical services and innovative solutions to improve the health and
-          well-being of our patients.
-        </p>
-      </div>
-      <img
-        src="/images/about/about-hero.png"
-        alt="About Us"
-        className={styles.image}
-      />
-    </section>
+    <SharedGradient className={styles.hero}>
+      <Container className={styles.layout}>
+        <div className={styles.content}>
+          <SectionIntro animateTitle />
+
+          <div className={styles.actions}>
+            <Button text="Book a Consultation" variant="filled" width="222px" />
+            <Button text="View Our Work" variant="transparent" width="147px" />
+          </div>
+        </div>
+
+        <img
+          className={styles.image}
+          src="/images/about/about-hero.png"
+          alt="hero-doc"
+        />
+      </Container>
+    </SharedGradient>
   );
 }
+
 export default Hero;
