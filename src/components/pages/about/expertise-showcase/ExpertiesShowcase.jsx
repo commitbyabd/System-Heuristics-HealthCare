5a2f667531c2e3830c7fb89e643baa9620016635
muhammad-abdullah777 <muhammadabdullahamjad@gmail.com>
2026-05-01 23:02:46 +0500

    Added the correct files for expertise section

---
+++ src/components/pages/about/expertise-showcase/ExpertiesShowcase.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./experties-showcase.module.css";
+function ExpertiesShowcase() {
+  return <div>ExpertiesShowcase</div>;
+}
+
+export default ExpertiesShowcase;
