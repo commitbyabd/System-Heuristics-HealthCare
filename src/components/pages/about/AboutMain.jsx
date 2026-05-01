4f82f18b99bf38129f823e9b1b6be561082a6deb
muhammad-abdullah777 <muhammadabdullahamjad@gmail.com>
2026-05-01 15:02:12 +0500

    principles files made

--- src/components/pages/about/AboutMain.jsx
+++ src/components/pages/about/AboutMain.jsx
@@ -1,9 +1,11 @@
 import Hero from "../about/hero/Hero";
+import Principles from "./principles/Principles";
 function AboutMain() {
   return (
-    <section>
+    <div>
       <Hero />
-    </section>
+      <Principles />
+    </div>
   );
 }
 
--- src/components/pages/about/principles/Principles.jsx
+++ src/components/pages/about/principles/Principles.jsx
@@ -1,2 +1,24 @@
 import styles from "./principles.module.css";
 import Container from "../../../ui/container/Container";
+import SectionIntro from "../../../ui/section-intro/SectionIntro";
+
+import React from "react";
+
+function Principles() {
+  return (
+    <div className={styles.content}>
+      <SectionIntro
+        title="Our Principles"
+        description="The foundation of everything we do, our mission, vision, and values guide how we build
+technology. empower people, and shape the future."
+        highlightWord={2}
+        className={styles.intro}
+        titleClassName={styles.introTitle}
+        descriptionClassName={styles.introDescription}
+        animateTitle
+      />
+    </div>
+  );
+}
+
+export default Principles;
